d4f79ffe2db3cadc61ec0e80ba9f539aef170eb1
AdamWatts <AdamT.Watts@gmail.com>
2018-04-28 19:40:45 -0700

    ToDo-app

---
+++ assets/JS/main.js
@@ -0,0 +1,70 @@
+// Refactor for best code
+
+// Define global const's
+const ul = document.querySelector('.todo-list');
+const input = document.querySelector('.add-text[type="text"]');
+const btn = document.querySelector('.heading-btn');
+const criteria = {
+  isComplete: el => el.classList.contains,
+  isRemove: el => el && el.matches('a'),
+  isAdd: el => event.keyCode === 13,
+  isInput: el => input.style.display === 'none'
+};
+
+// create helper functions
+completeHandler = event => {
+  const el = event.target;
+  if (criteria.isComplete(el)) {
+    event.stopPropagation();
+    el.classList.toggle('completed');
+  } else {
+    event.preventDefault();
+  }
+}
+
+removeHandler = event => {
+  const el = event.target;
+  //const li = document.getElementById('li');
+  if (criteria.isRemove(el)) {
+    event.stopPropagation();
+    //li.className += 'faded';
+    el.parentNode.parentNode.removeChild(el.parentNode);
+  } else {
+    event.preventDefault();
+  }
+}
+
+addHandler = event => {
+  const el = event.target;
+  const li = document.createElement('li');
+  const a = document.createElement('a');
+  const textInput = document.querySelector('.add-text');
+  const text = textInput.value;
+  li.innerHTML = text;
+  a.setAttribute('class', 'todo-list__link');
+  a.setAttribute('href', '#');
+  a.innerHTML = '&times';
+  li.setAttribute('class', 'todo-list__item');
+  if (criteria.isAdd(el)) {
+    event.stopPropagation();
+    ul.appendChild(li).appendChild(a);
+    input.value = '';
+  }
+}
+
+inputHandler = event => {
+  el = event.target;
+  if (criteria.isInput(el)) {
+    input.style.display = 'block';
+    event.stopPropagation();
+  } else {
+    input.style.display = 'none';
+    event.preventDefault();
+  }
+}
+
+// call eventListeners
+ul.addEventListener('click', completeHandler, false);
+ul.addEventListener('click', removeHandler, false);
+input.addEventListener('keydown', addHandler, false);
+btn.addEventListener('click', inputHandler, false);
