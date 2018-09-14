38323b0ba45570e444b146bfdc3f03130013e6af
Adam Watts <AdamT.Watts@Gmail.com>
2018-09-14 14:28:42 -0700

    cleaned up comments

--- assets/JS/main.js
+++ assets/JS/main.js
@@ -53,11 +53,9 @@ inputHandler = event => {
   el = event.target;
   if (criteria.isInput(el)) {
     input.style.display = 'block';
-    // btn.style.display = 'open'
     event.stopPropagation();
   } else {
     input.style.display = 'none';
-    // btn.style.display = 'close'
     event.preventDefault();
   }
 }
